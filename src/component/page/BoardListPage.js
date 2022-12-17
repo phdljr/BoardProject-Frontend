d8d1d08bf576bc0c5762d2a07473f16e2d7e901a
phdljr <phdljr@naver.com>
2022-12-18 02:27:55 +0900

    feat: pagination 추가

--- src/component/page/BoardListPage.js
+++ src/component/page/BoardListPage.js
@@ -1,25 +1,73 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 
 export default function BoardListPage() {
     const [boardList, setBoardList] = useState([]);
+    const [pageList, setPageList] = useState([]);
+    const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
     useEffect(() => {
         axios.get("http://localhost:8080/board")
             .then(res => {
-                setBoardList(res.data)
+                setBoardList(res.data.boardList)
+                setPageList(res.data.pageList)
+                setCurrentPageNumber(res.data.currentPageNumber)
                 console.log(res.data)
             })
     }, [])
 
+    function formatDate(date) {
+        let result = "";
+        for (let i = 0; i < 2; i++) {
+            result += date[i]
+            result += "-"
+        }
+        result += date[2] + " "
+
+        for (let i = 3; i < 5; i++) {
+            result += date[i]
+            result += ":"
+        }
+        result += date[5]
+        return result
+    }
+
     return (
         <>
-            {boardList.map((board, index) => (
-                <div key={index}>
-                    <span>{board.id}</span>
-                    <span>{board.title}</span>
-                </div>
-            ))}
+            <Table hover>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>날짜</th>
+                        <th>조회수</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {boardList.map((board) => (
+                        <tr key={board.id}>
+                            <td>{board.id}</td>
+                            <td>{board.title}</td>
+                            <td>{board.nickname}</td>
+                            <td>{formatDate(board.registerDate)}</td>
+                            <td>{board.hit}</td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </Table>
+            <Pagination style={{ justifyContent: "center" }}>
+                <Pagination.First />
+                <Pagination.Prev />
+                {pageList.map((page, index) => (
+                    <Pagination.Item key={index} active={page === currentPageNumber ? true : false}>{page}</Pagination.Item>
+                ))}
+                <Pagination.Next />
+                <Pagination.Last />
+            </Pagination>
         </>
     )
 }
